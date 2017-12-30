b7222c68f28c05eb5bc61cf1b63ae4427c026c0e
medvedz <russianbear152@gmail.com>
2017-12-30 18:44:26 +0300

    $

--- Filters_project/Filters.py
+++ Filters_project/Filters.py
@@ -119,49 +119,47 @@ class Filters(object):
                 resimg.picture[x][y] = R/(m.add_k())
         return resimg
 
-    def SpaceFilter_notline(self,n,flag=0):  # 0- median  1- min  2- max
+    def SpaceFilter_notline(self,n,flag=0):  # 0- median  1- erosion   2- building up
         resimg=copy.deepcopy(self)
-        m = Masks.Mask(n)
-        if (flag==0):
+
+        if (flag == 0):
+            m = Masks.Mask(n)
             m.fill_one()
-        else:
-            m.fill_circle()
-        a=list()
+            a=list()
 
 
-        start_h = int((m.heigth - 1) / 2)
-        start_w = int((m.width - 1) / 2)
+            start_h = int((m.heigth - 1) / 2)
+            start_w = int((m.width - 1) / 2)
 
-        end_h = resimg.heigth - start_h
-        end_w = resimg.width - start_w
+            end_h = resimg.heigth - start_h
+            end_w = resimg.width - start_w
 
-        for x in range(start_h, end_h):
-            for y in range(start_w, end_w):
-                a.clear()
-                for i in range(1,int((m.heigth+1) / 2)):
-                    for j in range(1,int((m.width+1) / 2)):
-                        if (m.mask[start_h + i][start_w + j]  !=0 ):
-                            a.append(resimg.picture[x + i][y + j])
-                        if (m.mask[start_h - i][start_w + j]  != 0):
-                            a.append(resimg.picture[x - i][y + j])
-                        if (m.mask[start_h + i][start_w - j]  != 0):
-                            a.append(resimg.picture[x + i][y - j])
-                        if (m.mask[start_h - i][start_w - j]  != 0):
-                            a.append(resimg.picture[x - i][y - j])
-                a.append(resimg.picture[x][y])
-                for i in range(1,int((m.heigth + 1) / 2)):
-                    a.append(resimg.picture[x+i][y])
-                    a.append(resimg.picture[x-i][y])
-                for j in range(1,int((m.width + 1) / 2)):
-                    a.append(resimg.picture[x][y+j])
-                    a.append(resimg.picture[x][y-j])
-                a = sorted(a)
-                if (flag == 0):         # mid
+            for x in range(start_h, end_h):
+                for y in range(start_w, end_w):
+                    a.clear()
+                    for i in range(1,int((m.heigth+1) / 2)):
+                        for j in range(1,int((m.width+1) / 2)):
+                            if (m.mask[start_h + i][start_w + j]  !=0 ):
+                                a.append(resimg.picture[x + i][y + j])
+                            if (m.mask[start_h - i][start_w + j]  != 0):
+                                a.append(resimg.picture[x - i][y + j])
+                            if (m.mask[start_h + i][start_w - j]  != 0):
+                                a.append(resimg.picture[x + i][y - j])
+                            if (m.mask[start_h - i][start_w - j]  != 0):
+                                a.append(resimg.picture[x - i][y - j])
+                    a.append(resimg.picture[x][y])
+                    for i in range(1,int((m.heigth + 1) / 2)):
+                        a.append(resimg.picture[x+i][y])
+                        a.append(resimg.picture[x-i][y])
+                    for j in range(1,int((m.width + 1) / 2)):
+                        a.append(resimg.picture[x][y+j])
+                        a.append(resimg.picture[x][y-j])
+                    a = sorted(a)
                     resimg.picture[x][y]=a[int((m.heigth*m.width)/2)]
-                if (flag == 1):         # min
-                    resimg.picture[x][y]=min(a)
-                if (flag == 2):         # max
-                    resimg.picture[x][y]=max(a)
+        if (flag == 1):   # эрозия
+             resimg=resimg.Filters_morf(n,0)
+        if (flag == 2):   # наращивание
+             resimg=resimg.Filters_morf(n,1)
         return resimg
 
     def SpaceFilter_laplacian(self,flag=0):     # 0 - без диагональных элементов   1 - с диагональными элементами
@@ -204,5 +202,58 @@ class Filters(object):
                 dx = mt.fabs((a.picture[i+1][j-1]+2*a.picture[i+1][j]+a.picture[i+1][j+1])-(a.picture[i-1][j-1]+2*a.picture[i-1][j]+a.picture[i-1][j+1]))
                 dy = mt.fabs((a.picture[i - 1][j + 1] + 2 * a.picture[i ][j+1] + a.picture[i + 1][j + 1]) - (a.picture[i - 1][j - 1] + 2 * a.picture[i ][j-1] + a.picture[i + 1][j - 1]))
                 resimg.picture[i][j]=dx+dy
+
+        return resimg
+
+    def Filters_morf(self,n=3,flag=0):      # falg == 0 - erosion (max), flag == 1 - наращивание (min)
+        resimg=copy.deepcopy(self)
+        A=copy.deepcopy(self)
+
+        m=Masks.Mask(n)
+        m.fill_one()
+
+        start_h = int((m.heigth - 1) / 2)
+        start_w = int((m.width - 1) / 2)
+
+        end_h = resimg.heigth - start_h
+        end_w = resimg.width - start_w
+
+        a=list()
+
+        for x in range(start_h, end_h):
+            for y in range(start_w, end_w):
+                a.clear()
+                for i in range(1, int((m.heigth + 1) / 2)):
+                    for j in range(1, int((m.width + 1) / 2)):
+                        if (m.mask[start_h + i][start_w + j] != 0):
+                            a.append(A.picture[x + i][y + j])
+                        if (m.mask[start_h - i][start_w + j] != 0):
+                            a.append(A.picture[x - i][y + j])
+                        if (m.mask[start_h + i][start_w - j] != 0):
+                            a.append(A.picture[x + i][y - j])
+                        if (m.mask[start_h - i][start_w - j] != 0):
+                            a.append(A.picture[x - i][y - j])
+                a.append(A.picture[x][y])
+                for i in range(1, int((m.heigth + 1) / 2)):
+                    a.append(A.picture[x + i][y])
+                    a.append(A.picture[x - i][y])
+                for j in range(1, int((m.width + 1) / 2)):
+                    a.append(A.picture[x][y + j])
+                    a.append(A.picture[x][y - j])
+                a = sorted(a)
+
+                for i in range(m.heigth):
+                    for j in range(m.width):
+                        if (flag==0):  # erosion
+                            if (A.picture[x-int((m.heigth - 1) / 2)+i][y-int((m.width - 1) / 2)+j]==max(a)):
+                                for i1 in range(m.heigth):
+                                    for j1 in range(m.width):
+                                        resimg.picture[x - int((m.heigth - 1) / 2) + i1][y - int((m.width - 1) / 2) + j1] = max(a)
+                        if (flag==1):  # наращивание
+                            if (A.picture[x-int((m.heigth - 1) / 2)+i][y-int((m.width - 1) / 2)+j]==min(a)):
+                                for i1 in range(m.heigth):
+                                    for j1 in range(m.width):
+                                        resimg.picture[x - int((m.heigth - 1) / 2) + i1][y - int((m.width - 1) / 2) + j1] = min(a)
+
 
         return resimg
